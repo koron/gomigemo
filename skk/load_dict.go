5f0c251085d7e31d4a5a2fbdff8ea5398cc436b4
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-06-16 22:41:27 +0900

    impl SKK dict parser

--- skk/load_dict.go
+++ skk/load_dict.go
@@ -1,7 +1,9 @@
 package skk
 
 import (
+	"fmt"
 	"github.com/koron/gomigemo/readutil"
+	"strings"
 )
 
 type Word struct {
@@ -22,12 +24,42 @@ func LoadDict(path string, proc DictEntryProc) error {
 		if err2 != nil {
 			return err2
 		}
-		proc(entry)
+		if entry != nil {
+			proc(entry)
+		}
 		return err
 	})
 }
 
 func line2entry(line string) (entry *DictEntry, err error) {
-	// TODO: Convert SKK dict line (string) to entry.
-	return nil, nil
+	// Convert SKK dict line (string) to entry.
+	if strings.HasPrefix(line, ";;") {
+		return nil, nil
+	}
+	line = strings.TrimRight(line, " \t\r\n")
+	items := strings.SplitN(line, " ", 2)
+	if items == nil || len(items) != 2 {
+		return nil, fmt.Errorf("Invalid format")
+	}
+	label := items[0]
+	values := strings.Split(strings.Trim(items[1], "/"), "/")
+	words := make([]Word, len(values))
+	for i, v := range values {
+		words[i] = value2word(v)
+	}
+	return &DictEntry{
+		Label: label,
+		Words: words,
+	}, nil
+}
+
+func value2word(v string) Word {
+	n := strings.Index(v, ";")
+	if n < 0 {
+		return Word{Text: v}
+	}
+	return Word{
+		Text: v[0:n],
+		Desc: v[n+1:],
+	}
 }
