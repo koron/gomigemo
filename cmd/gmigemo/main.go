a5efaebe8d3bacff00bd9a0fb980bfd389672944
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-06-26 16:50:12 +0900

    impl query loop

--- cmd/gmigemo/main.go
+++ cmd/gmigemo/main.go
@@ -1,17 +1,58 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/koron/gomigemo/migemo"
 	"log"
+	"os"
+	"strings"
 )
 
 func dictdir() string {
 	return "./_dict"
 }
 
+func setupOption(m migemo.Matcher) {
+	o := m.GetOptions()
+	o.OpWSpaces = ""
+	m.SetOptions(o)
+}
+
 func query(d migemo.Dict, s string) (string, error) {
-	return migemo.Pattern(d, s)
+	m, err := d.Matcher(s)
+	if err != nil {
+		return "", err
+	}
+	setupOption(m)
+	p, err := m.Pattern()
+	if err != nil {
+		return "", err
+	}
+	return p, nil
+}
+
+func readInput(ch chan<- string) {
+	r := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("QUERY: ")
+		l, err := r.ReadString('\n')
+		if l != "" {
+			ch <- strings.TrimSpace(l)
+		}
+		if err != nil {
+			return
+		}
+	}
+}
+
+func openQueries() <-chan string {
+	ch := make(chan string, 1)
+	go func() {
+		readInput(ch)
+		close(ch)
+	}()
+	return ch
 }
 
 func main() {
@@ -19,12 +60,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	for q := range cl {
+	ch := openQueries()
+	for q := range ch {
 		p, err := query(dict, q)
 		if err != nil {
-			log.Error(err)
+			log.Print(err)
 			continue
 		}
-		fmt.Println("PATTERN: %s\n", p)
+		fmt.Printf("PATTERN: %s\n", p)
 	}
 }
