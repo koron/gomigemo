4995bf96047c09dfb5b86c51c957c6be43be93f9
MURAOKA Taro <taro.muraoka@muraoka-design.com>
2014-06-30 12:04:57 +0900

    add migemogrep cmd, from https://gist.github.com/mattn/6d745b78ee920630996a

---
+++ cmd/migemogrep/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"regexp"
+
+	"github.com/koron/gomigemo/migemo"
+)
+
+func dictdir() string {
+	d := os.Getenv("GMIGEMO_DICTDIR")
+	if d != "" {
+		return d
+	}
+	d = os.Getenv("GOPATH")
+	if d == "" {
+		return "./_dict"
+	}
+	return filepath.Join(d, "src", "github.com", "koron", "gomigemo", "_dict")
+}
+
+var dictPath = flag.String("d", dictdir(),  "Location to dictionary")
+
+func grep(r io.Reader, re *regexp.Regexp) error {
+	buf := bufio.NewReader(r)
+	for {
+		b, _, err := buf.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+		line := string(b)
+		if re.MatchString(line) {
+			fmt.Println(line)
+		}
+	}
+	return nil
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage of main: [pattern] [files...]")
+	}
+
+	flag.Parse()
+
+	if flag.NArg() != 1 && flag.NArg() != 2 {
+		flag.Usage()
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	dict, err := migemo.Load(*dictPath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	re, err := migemo.Compile(dict, flag.Arg(0))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if flag.NArg() == 1 {
+		if err = grep(os.Stdin, re); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	} else {
+		for _, arg := range flag.Args()[1:] {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			defer f.Close()
+			if err = grep(f, re); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		}
+	}
+}
