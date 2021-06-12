1c0d68acef2e3137946bebd2639c11cd6b0df789
MURAOKA Taro <koron.kaoriya@gmail.com>
2021-06-13 01:33:47 +0900

    use go:embed for Go 1.16

---
+++ embedict/fs.go
@@ -0,0 +1,32 @@
+// +build go1.16
+
+package embedict
+
+import (
+	"embed"
+	"fmt"
+	"io"
+	"io/fs"
+)
+
+//go:embed _dict
+var dictFS embed.FS
+
+var DictFS fs.FS
+
+func init() {
+	fs, err := fs.Sub(dictFS, "_dict")
+	if err != nil {
+		panic(err)
+	}
+	DictFS = fs
+}
+
+func Asset(name string) ([]byte, error) {
+	f, err := DictFS.Open(name)
+	if err != nil {
+		return nil, fmt.Errorf("Asset %q not found: %w", name, err)
+	}
+	defer f.Close()
+	return io.ReadAll(f)
+}
